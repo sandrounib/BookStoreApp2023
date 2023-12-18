3a279ed5197740ace72804631a0eefb9eb83e94a
sandro.reis <t.sandro.reis@bankly.com.br>
2023-12-17 22:43:46 -0300

    feat: dev api=extensions

--- BookStore.Api/Program.cs
+++ BookStore.Api/Program.cs
@@ -3,9 +3,10 @@ using BookStore.Api.Extensions;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-//builder.Services.AddSwaggerGen();
-//builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddDatabase(builder.Configuration);
+//builder.Services.AddMediatorExtension();
 
 var app = builder.Build();
 
